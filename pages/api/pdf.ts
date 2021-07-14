39d62d321baa3794bebd36d87f5259dbdc23d368
Jeff Wong <jeff@richmonkeys.com>
2021-07-14 21:14:01 +0800

    backend usage example

--- pages/api/pdf.ts
+++ pages/api/pdf.ts
@@ -1,6 +1,7 @@
 import parseBody, { BusboyFile } from '@richmonkeys/parse-body'
 import { NextApiRequest, NextApiResponse } from 'next'
 import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf'
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/legacy/build/pdf.worker'
 
 export const config = { api: { bodyParser: false } }
 
@@ -19,9 +20,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(400).end('Multipart/form-data "pdf" of type "file" is required.')
   }
 
+  if (body.pdf.mimetype !== 'application/pdf') {
+    return res.status(400).end('Multipart/form-data "pdf" must be of content-type "application/pdf".')
+  }
+
   const pdf = await pdfjsLib.getDocument(body.pdf.data).promise
-  
+
   res.send({
     numPages: pdf.numPages,
+    mimetype: body.pdf.mimetype,
   })
 }
